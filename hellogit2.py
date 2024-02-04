6f2009ee20daa6320d40c40496e3f3725f67ce80
hectoralonzo2702 <hectoralonzo2702@gmail.com>
2024-02-04 05:43:23 -0800

    Hello Git dos (ds dos dos...) + probando cambios...

--- hellogit2.py
+++ hellogit2.py
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 print("Hello Git dosssss (222222222222222) + cambio probando cambios...!")
-=======
-print("Hello Git dos (20002000) + probando cambios...!")
->>>>>>> 4e568f01f1bbccd0e3e6ac5b728d1ed528175184
